309e8e8c60796cd0418dc5843414598b3dea03f9
Arnau Miro <arnau.miro@upc.edu>
2021-07-19 13:49:09 +0200

    Added missing __init__.py

---
+++ pyLOM/utils/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+#
+# pyLOM - Python Low Order Modeling.
+#
+# Utils Module
+#
+# Last rev: 19/07/2021
+
+__VERSION__ = '1.0.0'
+
+from .errors import raiseError, raiseWarning
+from .cr     import cr_start, cr_stop, cr_info
+
+del errors, cr
