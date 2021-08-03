07a405bd7660b02d75fe84beaf1cd511b85d49d4
Arnau Miro <arnau.miro@upc.edu>
2021-08-03 17:49:10 +0200

    plotting tools

---
+++ pyLOM/utils/plotting.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+#
+# pyLOM - Python Low Order Modeling.
+#
+# Plotting - Plot routines.
+#
+# Last rev: 03/08/2021
+from __future__ import print_function, division
+
+
+
+
+def plotResidual(S,fig=None,ax=None):
+	'''
+	'''
+	pass
+
+
+def plotMode(U,VT,mesh,fig=None,ax=None):
+	'''
+	'''
+	pass
+
+
+def plotSnapshot(X,mesh,fig=None,ax=None):
+	'''
+	'''
+	pass
