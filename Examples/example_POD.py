d5c7217dd9e6c5f71dca85690548da59b39d291e
Arnau Miro <arnau.miro@upc.edu>
2021-08-04 12:57:46 +0200

    Updated examples accordingly

--- Examples/example_POD.py
+++ Examples/example_POD.py
@@ -6,7 +6,6 @@
 from __future__ import print_function, division
 
 import numpy as np
-import matplotlib.pyplot as plt
 
 import pyLOM
 
@@ -22,27 +21,22 @@ t  = d.time
 dt = d.time[1] - d.time[0]
 
 
-## Compute POD after subtracting mean (i.e., do PCA)
+## Compute POD
 pyLOM.cr_start('example',0)
 # Run POD
-PSI,S,V = pyLOM.POD.run(X,r=1e-6) # PSI are POD modes
+PSI,S,V = pyLOM.POD.run(X) # PSI are POD modes
+pyLOM.plotResidual(S)
+# Truncate according to a residual
+PSI,S,V = pyLOM.POD.truncate(PSI,S,V,r=5e-6)
 # Obtain PSD of the first mode
 PSD,freq = pyLOM.POD.PSD(V,dt,m=mode) 
 pyLOM.cr_stop('example',0)
 
 
-## Plots
-fig, ax = plt.subplots(3,1,figsize=(8,6),dpi=100,facecolor='w',edgecolor='k',gridspec_kw = {'hspace':0.5})
-# Plot POD temporal mode
-ax[1].plot(t[:S.shape[0]],V[0,:],'b')
-ax[1].set_title('POD temporal mode m=%d'%mode)
-# Plot FFT of V
-L = int(np.floor(V.shape[0]/2))
-ax[2].plot(freq[:L],PSD[:L])
-ax[2].set_title('Power Spectrum')
-ax[2].set_xlabel('St')
+## Plot POD mode
+plotMode(PSI,V,None,mode=mode)
 
 
 ## Show and print timings
 pyLOM.cr_info()
-plt.show()
+pyLOM.show_plots()
--- Examples/example_POD_matlab.py
+++ Examples/example_POD_matlab.py
@@ -14,57 +14,52 @@ import pyLOM
 ## Data loading
 d  = pyLOM.Dataset.load('DATA/CYLINDER.h5')
 X  = d['UALL']
-N  = 151
+
+fig, ax = plt.subplots(3,1,figsize=(8,6),dpi=100,facecolor='w',edgecolor='k',gridspec_kw = {'hspace':0.5})
 
 
-## Compute POD after subtracting mean (i.e., do PCA)
+## Compute POD
 pyLOM.cr_start('example',0)
+# Compute and substract temporal mean
 Uavg = pyLOM.POD.temporal_mean(X)
 X_m  = pyLOM.POD.subtract_mean(X,Uavg)
 Y    = X_m
 
-PSI,S,V = pyLOM.POD.svd(Y,transpose_v=False)
-pyLOM.cr_stop('example',0) # PSI are POD modes
+# Compute SVD
+PSI,S,V = pyLOM.POD.svd(Y)
 
 # Plot accumulative S
-plt.figure(figsize=(8,6),dpi=100,facecolor='w',edgecolor='k')
+pyLOM.plotResidual(S)
 
-accumulative_S = np.zeros((N,))
-for ii in range(N):
-    accumulative_S[ii] = np.linalg.norm(S[ii:N],2)
-accumulative_S = accumulative_S/np.linalg.norm(S,2)
+# Truncate according to residual
+N,res = pyLOM.POD.residual(S,r=5e-6)
+print('POD: truncating at %d with a residual of %.2e'%(N,res))
+PSI = PSI[:,:N]
+S   = S[:N]
+V   = V[:N,:]
 
-plt.semilogy(np.linspace(1, N, N), np.transpose(accumulative_S), 'bo')
-plt.ylabel('varepsilon1')
-plt.xlabel('Truncation size')
-plt.title('Tolerance')
-plt.ylim((0, 1))
-
-
-## V representation
-fig, ax = plt.subplots(3,1,figsize=(8,6),dpi=100,facecolor='w',edgecolor='k',gridspec_kw = {'hspace':0.5})
-
-pyLOM.cr_start('example',0)
+# V representation
 dt = 0.2;
 t  = dt*np.arange(V.shape[1])
 m  = 1 # POD temporal mode number
 y  = V[m-1,:]
-pyLOM.cr_stop('example',0) # PSI are POD modes
 ax[1].plot(t,y,'b')
 ax[1].set_title('POD temporal mode m=%d'%m)
 
-
-## Fast Fourier Transform of V
-pyLOM.cr_start('example',0)
+# Fast Fourier Transform of V
 PSD  = pyLOM.POD.power_spectral_density(y)
 freq = 1./dt/y.shape[0]*np.arange(y.shape[0])
-L = int(np.floor(V.shape[0]/2))
-pyLOM.cr_stop('example',0) # PSI are POD modes
+L    = int(np.floor(V.shape[0]/2))
 ax[2].plot(freq[:L],PSD[:L])
 ax[2].set_title('Power Spectrum')
 ax[2].set_xlabel('St')
 
+# Reconstruction
+X_POD = np.matmul(np.matmul(PSI,S*np.identity(S.shape[0],np.double)),V)
+
+pyLOM.cr_stop('example',0)
+
 
 ## Show and print timings
 pyLOM.cr_info()
-plt.show()
+pyLOM.show_plots()
