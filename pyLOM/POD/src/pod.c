8314c199716714a81050e7e12023eb35d33ea931
Benet Eiximeno Franch <beneteiximenofranch@hotmail.com>
2021-09-17 00:50:51 +0200

    changes

--- pyLOM/POD/src/pod.c
+++ pyLOM/POD/src/pod.c
@@ -1,7 +1,7 @@
 /*
 	POD - C Functions to compute POD
 */
-
+#define USE_LAPACK_DGESVD
 #include <stdlib.h>
 #include <complex.h>
 #include <math.h>
@@ -280,7 +280,7 @@ void compute_reconstruct_svd(double *X, double *Ur, double *Sr, double *VTr, con
 double compute_RMSE(double *X_POD, double *X, const int m, const int n) {
 	/*
 		Compute and return the Root Meean Square Error and returns it
-		
+
 		X_POD(m, n) is the flow reconstructed with truncated matrices
 		X(m,n) is the flow reconstructed
 	*/
