273c043b5207bb9ca68dc43cd06787ff19d5df14
Benet Eiximeno Franch <beneteiximenofranch@hotmail.com>
2021-09-17 00:48:36 +0200

    changes

--- Examples/example_POD_jetLES.py
+++ Examples/example_POD_jetLES.py
@@ -10,8 +10,8 @@ import numpy as np
 import pyLOM
 
 ## Parameters
-DATAFILE = 'DATA/jetLES.h5'
-mode     = 1
+DATAFILE = 'Examples/Data/jetLES.h5'
+mode     = 3
 
 
 ## Data loading
@@ -22,16 +22,22 @@ dt = d.time[1] - d.time[0]
 
 
 ## Compute POD
-pyLOM.cr_start('example',0)
+#pyLOM.cr_start('example',0)
 # Run POD
 PSI,S,V = pyLOM.POD.run(X) # PSI are POD modes
+print('PSI ', PSI)
+print('S ', S)
+print('V ', V)
+
 pyLOM.plotResidual(S)
 # Truncate according to a residual
+'''
 PSI,S,V = pyLOM.POD.truncate(PSI,S,V,r=5e-6)
 # Obtain PSD of the first mode
 PSD,freq = pyLOM.POD.PSD(V,dt,m=mode)
 # Reconstruct the flow
 X_POD = pyLOM.POD.reconstruct(PSI,S,V)
+print(X_POD.shape)
 # Compute RMSE
 rmse = pyLOM.POD.RMSE(X_POD,X)
 pyLOM.cr_stop('example',0)
@@ -40,8 +46,9 @@ print('RMSE = %.2e'%rmse)
 
 
 ## Plot POD mode
-pyLOM.plotMode(PSI,V[0,:],PSD,t,d.mesh)
-
+pyLOM.plotSnapshot(X_POD[:,10],d.xyz,d.mesh)
+pyLOM.plotMode(PSI[:,mode-1],d.xyz,V[mode-1,:],PSD, t,d.mesh)
+'''
 
 ## Show and print timings
 pyLOM.cr_info()
