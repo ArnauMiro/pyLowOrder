98a4b231508aca878bdddbb6874089c4e29a4ead
Arnau Miro <arnau.miro@upc.edu>
2021-09-16 18:11:59 +0200

    Code improvements

--- pyLOM/POD/src/pod.c
+++ pyLOM/POD/src/pod.c
@@ -3,6 +3,7 @@
 */
 
 #include <stdlib.h>
+#include <stdio.h>
 #include <complex.h>
 #include <math.h>
 
@@ -60,7 +61,7 @@ void subtract_temporal_mean(double *out, double *X, double *X_mean, const int m,
 	#ifdef USE_OMP
 	#pragma omp parallel for collapse(2) shared(out,X,X_mean) firstprivate(m,n)
 	#endif
-	for(int ii=0; ii<m; ++ii){
+	for(int ii=0; ii<m; ++ii) {
 		for(int jj=0; jj<n; ++jj)
 			AC_OUT(ii,jj) = AC_X(ii,jj) - X_mean[ii];
 	}
@@ -82,12 +83,14 @@ void single_value_decomposition(double *U, double *S, double *VT, double *Y, con
 			http://www.netlib.org/lapack//explore-html/d3/d23/lapacke__dgesdd_8c_aaf227f107a19ae6021f591c4de5fdbd5.html
 		On ROW/COL major:
 			https://stackoverflow.com/questions/34698550/understanding-lapack-row-major-and-lapack-col-major-with-lda
+			https://software.intel.com/sites/products/documentation/doclib/mkl_sa/11/mkl_lapack_examples/lapacke_dgesvd_row.c.htm
 	*/
+	int info, mn = MIN(m,n);
 	#ifdef USE_LAPACK_DGESVD
 	// Run LAPACKE DGESVD for the single value decomposition
 	double *superb;
-	superb = (double*)malloc((int)(MIN(m,n)-1)*sizeof(double));
-	LAPACKE_dgesvd(
+	superb = (double*)malloc((mn-1)*sizeof(double));
+	info = LAPACKE_dgesvd(
 		LAPACK_ROW_MAJOR, // int  		matrix_layout
 					 'S', // char  		jobu
 					 'S', // char  		jobvt
@@ -97,7 +100,7 @@ void single_value_decomposition(double *U, double *S, double *VT, double *Y, con
 					   n, // int  		lda
 					   S, // double *  	s
 					   U, // double *  	u
-					   n, // int  		ldu
+					  mn, // int  		ldu
 					  VT, // double *  	vt
 					   n, // int  		ldvt
 				  superb  // double *  	superb
@@ -105,7 +108,7 @@ void single_value_decomposition(double *U, double *S, double *VT, double *Y, con
 	free(superb);
 	#else
 	// Run LAPACKE DGESDD for the single value decomposition
-	LAPACKE_dgesdd(
+	info = LAPACKE_dgesdd(
 		LAPACK_ROW_MAJOR, // int  		matrix_layout
 					 'S', // char  		jobz
 					   m, // int  		m
@@ -114,11 +117,15 @@ void single_value_decomposition(double *U, double *S, double *VT, double *Y, con
 					   n, // int  		lda
 					   S, // double *  	s
 					   U, // double *  	u
-					   n, // int  		ldu
+					  mn, // int  		ldu
 					  VT, // double *  	vt
 					   n  // int  		ldvt
 	);
 	#endif
+	if( info > 0 ) {
+		printf("The algorithm computing SVD failed to converge.\n");
+		exit( 1 );
+	}
 }
 
 int compute_truncation_residual(double *S, double res, const int n) {
