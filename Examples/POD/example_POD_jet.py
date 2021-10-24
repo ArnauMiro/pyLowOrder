22dbda05c41eb55e5aed1daf9cf246a88a4174a8
Arnau Miro <arnau.miro@upc.edu>
2021-10-24 23:05:21 +0200

    Updated Example

--- Examples/POD/example_POD_jet.py
+++ Examples/POD/example_POD_jet.py
@@ -5,17 +5,17 @@
 # Last revision: 19/07/2021
 from __future__ import print_function, division
 
-import numpy as np
+import os, numpy as np
 
 import pyLOM
 
 ## Parameters
 DATAFILE = './DATA/jetLES.h5'
-
+VARIABLE = 'PRESS'
 
 ## Data loading
 d = pyLOM.Dataset.load(DATAFILE)
-X  = d['p']
+X  = d[VARIABLE]
 t  = d.time
 dt = d.time[1] - d.time[0]
 
@@ -36,10 +36,33 @@ pyLOM.cr_stop('example',0)
 print('RMSE = %.2e'%rmse)
 
 
+## Dump to ParaView
+# Spatial modes
+d.add_variable('spatial_modes_P',True,6,0,d.extract_modes(PSI,1,modes=[1,4,6,2,5,3],point=d.info(VARIABLE)['point']))
+d.write('modes',basedir='out',instants=[0],vars=['spatial_modes_P'],fmt='ensi')
+pyLOM.io.Ensight_writeCase(os.path.join('out','modes.ensi.case'),'modes.ensi.geo',
+	[
+		{'name':'spatial_modes_P','dims':6,'point':d.info(VARIABLE)['point'],'file':'modes.ensi.spatial_modes_P-******'},
+	],
+	np.array([0.],np.double)
+)
+
+# Temporal evolution
+d.add_variable('PRESR',True,1,t.shape[0],X_POD)
+d.write('flow',basedir='out',instants=np.arange(t.shape[0],dtype=np.int32),vars=['PRESS','PRESR'],fmt='ensi')
+pyLOM.io.Ensight_writeCase(os.path.join('out','flow.ensi.case'),'flow.ensi.geo',
+	[
+		{'name':'PRESS','dims':1,'point':d.info(VARIABLE)['point'],'file':'flow.ensi.PRESS-******'},
+		{'name':'PRESR','dims':1,'point':d.info(VARIABLE)['point'],'file':'flow.ensi.PRESR-******'},
+	],
+	t
+)
+
+
 ## Plot POD mode
-pyLOM.plotMode(PSI,d.xyz,V,t,d.mesh,modes=[1,2,3,4])
+pyLOM.plotMode(PSI,d.xyz,V,t,d.mesh,d.info(VARIABLE),modes=[1,2,3,4],scale_freq=2.56)
 #pyLOM.plotSnapshot(X_POD[:,10],d.xyz,d.mesh)
-fig,ax,anim = pyLOM.animateFlow(X,X_POD,d.xyz,d.mesh,scale_freq=2.56)
+fig,ax,anim = pyLOM.animateFlow(X,X_POD,d.xyz,d.mesh,d.info(VARIABLE))
 
 
 ## Show and print timings
