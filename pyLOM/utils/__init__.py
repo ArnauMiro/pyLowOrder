6e0f9142c2c05d53105f5c0383b70ac7a06ebea5
Arnau Miro <arnau.miro@upc.edu>
2021-10-27 11:57:41 +0200

    Deleted matrix module from utils

--- pyLOM/utils/__init__.py
+++ pyLOM/utils/__init__.py
@@ -10,7 +10,6 @@ __VERSION__ = '1.0.0'
 
 from .errors import raiseError, raiseWarning
 from .cr     import cr_start, cr_stop, cr_info
-from .matrix import transpose, norm
 from .parall import MPI_RANK, MPI_SIZE, worksplit, is_rank_or_serial, pprint
 from .parall import mpi_scatter, mpi_gather, mpi_reduce
 
