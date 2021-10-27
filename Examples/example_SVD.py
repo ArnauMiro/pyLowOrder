Added eigenvalue computation and fixed cython issues

Examples/example_SVD.py
@@ -17,29 +17,25 @@ A = np.array([[1,2],[3,4],[5,6],[7,8]],dtype=np.double,order='C')
 ## Run SVD from numpy
 pyLOM.cr_start('SVD numpy',0)
 U, S, V = np.linalg.svd(A,full_matrices=False)
+pyLOM.cr_stop('SVD numpy',0)
 print('Numpy:')
 print(U.shape,U)
 print(S.shape,S)
 print(V.shape,V)
-pyLOM.cr_stop('SVD numpy',0)
 
 
 ## Run SVD from POD
-pyLOM.cr_start('SVD pyLOM',0)
 U, S, V = pyLOM.math.svd(A)
-print('POD:')
+print('pyLOM:')
 print(U.shape,U)
 print(S.shape,S)
 print(V.shape,V)
-pyLOM.cr_stop('SVD pyLOM',0)
 
-pyLOM.cr_start('TSQR_SVD pyLOM',0)
 U, S, V = pyLOM.math.tsqr_svd(A)
-print('POD:')
+print('pyLOM:')
 print(U.shape,U)
 print(S.shape,S)
 print(V.shape,V)
-pyLOM.cr_stop('TSQR_SVD pyLOM',0)
 
 
 pyLOM.cr_info()

Examples/example_eigen.py
@@ -21,20 +21,18 @@ A = np.array([
 ## Run eigenvalues from numpy
 pyLOM.cr_start('eigen numpy',0)
 w,v = np.linalg.eig(A)
+pyLOM.cr_stop('eigen numpy',0)
 print('Numpy:')
 print(w.shape,w)
 print(v.shape,v)
-pyLOM.cr_stop('eigen numpy',0)
 
 
 ## Run eigenvalyes from DMD
-pyLOM.cr_start('eigen cython',0)
-delta,w,v = pyLOM.DMD.eigen(A)
-print('DMD:')
+delta,w,v = pyLOM.math.eigen(A)
+print('pyLOM:')
 print(delta.shape,delta)
 print(w.shape,w)
 print(v.shape,v)
-pyLOM.cr_stop('eigen cython',0)
 
 
 pyLOM.cr_info()