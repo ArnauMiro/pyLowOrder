8e5ea61a93c636309aa6ab2f06a7d0ca13355e01
Benet Eiximeno Franch <beneteiximenofranch@hotmail.com>
2022-02-10 12:55:15 +0100

    tsqr_svd new in pythin

--- pyLOM/vmmath/wrapper.py
+++ pyLOM/vmmath/wrapper.py
@@ -10,7 +10,7 @@ from __future__ import print_function, division
 import numpy as np, scipy, nfft
 
 from ..utils.cr     import cr_start, cr_stop
-from ..utils.parall import MPI_RANK, mpi_gather, mpi_reduce
+from ..utils.parall import MPI_RANK, mpi_gather, mpi_reduce,
 from ..utils.errors import raiseError
 
 
@@ -30,7 +30,7 @@ def vector_norm(v,start=0):
 	'''
 	cr_start('math.vector_norm',0)
 	norm = np.linalg.norm(v[start:],2)
-	cr_stop('math.vector_norm',0)	
+	cr_stop('math.vector_norm',0)
 	return norm
 
 def matmul(A,B):
@@ -39,7 +39,7 @@ def matmul(A,B):
 	'''
 	cr_start('math.matmul',0)
 	C = np.matmul(A,B)
-	cr_stop('math.matmul',0)	
+	cr_stop('math.matmul',0)
 	return C
 
 def vecmat(v,A):
@@ -50,7 +50,7 @@ def vecmat(v,A):
 	C = np.zeros_like(A)
 	for ii in range(v.shape[0]):
 		C[ii,:] = v[ii]*A[ii,:]
-	cr_stop('math.vecmat',0)	
+	cr_stop('math.vecmat',0)
 	return C
 
 def eigen(A):
@@ -110,32 +110,129 @@ def svd(A):
 	cr_stop('math.svd',0)
 	return U,S,V
 
-def tsqr_svd(A):
+def tsqr_svd2(A):
 	'''
 	Single value decomposition (SVD) using Lapack.
 		U(m,n)   are the POD modes.
 		S(n)     are the singular values.
 		V(n,n)   are the right singular vectors.
 	'''
-	cr_start('math.tsqr_svd',0)
+	cr_start('math.tsqr_svd2',0)
 	# Algorithm 1 from Sayadi and Schmid (2016) - Q and R matrices
 	# QR factorization on A
 	Q1i, R = qr(A)
 	# Gather all Rs into Rp
 	Rp = mpi_gather(R,all=True)
 	# QR factorization on Rp
-	Q2i, R = np.linalg.qr(Rp)
+	Q2i, R = qr(Rp)
 	# Compute Q = Q1 x Q2
 	Q = matmul(Q1i,Q2i[A.shape[1]*MPI_RANK:A.shape[1]*(MPI_RANK+1),:])
-	
+
 	# Algorithm 2 from Sayadi and Schmid (2016) - Ui, S and VT
 	# At this point we have R and Qi scattered on the processors
 	# Call SVD routine
-	Ur, S, V = np.linalg.svd(R)
+	Ur, S, V = svd(R)
 	# Compute U = Q x Ur
-	U = np.matmul(Q,Ur)
-	cr_stop('math.tsqr_svd',0)
+	U = matmul(Q,Ur)
+	cr_stop('math.tsqr_svd2',0)
 	return U,S,V
+
+def next_power_of_2(n):
+	'''
+	Find the next power of 2 of n
+	'''
+	cr_start('math.next_power_of_2',0)
+    p = 1
+    if (n && !(n & (n - 1))): return n
+    while (p < n): p <<= 1
+	cr_stop('math.next_power_of_2',0)
+    return p
+
+def tsqr_svd(Ai):
+	'''
+	Single value decomposition (SVD) using TSQR algorithm from
+	J. Demmel, L. Grigori, M. Hoemmen, and J. Langou, ‘Communication-optimal Parallel
+	and Sequential QR and LU Factorizations’, SIAM J. Sci. Comput.,
+	vol. 34, no. 1, pp. A206–A239, Jan. 2012,
+
+	doi: 10.1137/080731992.
+
+	Ai(m,n)  data matrix dispersed on each processor.
+
+	Ui(m,n)  POD modes dispersed on each processor (must come preallocated).
+	S(n)     singular values.
+	VT(n,n)  right singular vectors (transposed).
+	'''
+	cr_start('math.tsqr_svd',0)
+	#Recover rank and size
+    MPI_RANK = MPI_COMM.Get_rank()
+    MPI_SIZE = MPI_COMM.Get_size()
+    m, n = Ai.shape
+	# Algorithm 1 from Demmel et al (2012)
+	# 1: QR Factorization on Ai to obtain Q1i and Ri
+    Q1i, R = qr(Ai)
+    nextPower = next_power_of_2(MPI_SIZE)
+    nlevels = np.log2(nextPower)
+    QW = np.eye(n, np.double)
+    C  = np.zeros((2*n, n), np.double)
+    belvel = 1
+    for ilevel in range(nlevels):
+		# Store R in the upper part of the C matrix
+        C[:n, :] = R
+		# Decide who sends and who recieves, use R as buffer
+        prank = MPI_RANK ^ blevel
+        if myproc & blevel:
+            if prank < MPI_SIZE:
+                mpi_send(R, prank)
+        else:
+            if prank < MPI_SIZE:
+                mpi_recv(R, prank)
+				# Store R in the lower part of the C matrix
+                C[n : -1, :] = R
+				# 2: QR from the C matrix, reuse C and R
+                Q2i, R = np.qr(C)
+				# Store Q2i from this level
+                Q2l[2*n*ilevel : 2*n*ilevel + 2*n, :] = Q2i
+        blevel <<= 1
+	# At this point R is correct on processor 0
+	# Broadcast R and its part of the Q matrix
+    blevel = 1 << (nlevels - 1)
+    for ilevel in reversed(range(nlevels)):
+        mask = blevel - 1
+        if MPI_RANK & mask == 0:
+			# Obtain Q2i for this level - use C as buffer
+            C = Q2l[2*n*ilevel : 2*n*ilevel + 2*n, :]
+			# Multiply by QW either set to identity or allocated to a value
+			# Store into Q2i
+            Q2i = matmul(C, QW)
+			# Communications scheme
+            prank = MPI_RANK^blevel
+            if MPI_RANK & blevel:
+                if prank < MPI_SIZE:
+                    mpi_recv(C, prank)
+					# Recover R from the upper part of C and QW from the lower part
+                    R = C[: n, :]
+                    QW = C[n : -1, :]
+            else:
+                if prank < MPI_SIZE:
+					# Set up C matrix for sending
+					# Store R in the upper part and Q2i on the lower part
+					# Store Q2i of this rank to QW
+                    C[: n, :] = R
+                    C[n : -1, :] = Q2i[n : -1, :]
+                    QW = Q2i[: n, :]
+                    mpi_send(C, prank)
+        blevel >>= 1
+	# Multiply Q1i and QW to obtain Qi
+	Qi = matmul(Q1i, QW)
+	# Algorithm 2 from Sayadi and Schmid (2016) - Ui, S and VT
+	# At this point we have R and Qi scattered on the processors
+	# Call SVD routine
+	Ur, S, V = svd(R)
+	# Compute Ui = Qi x Ur
+    Ui = matmul(Qi, Ur)
+	cr_stop('math.tsqr_svd',0)
+    return Ui S, V
 
 def fft(t,y,equispaced=True):
 	'''
@@ -146,7 +243,7 @@ def fft(t,y,equispaced=True):
 		ts = t[1] - t[0] # Sampling time
 		# Compute sampling frequency
 		f  = 1./ts/t.shape[0]*np.arange(t.shape[0],dtype=np.double)
-		# Compute power spectra using fft 
+		# Compute power spectra using fft
 		yf = scipy.fft.fft(y)
 	else:
 		# Compute sampling frequency
