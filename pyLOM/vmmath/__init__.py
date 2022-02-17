8b1879755211ec45cc6733e0eef3941d846c9222
Benet Eiximeno Franch <beneteiximenofranch@hotmail.com>
2022-02-17 13:09:16 +0100

    new functions used in dmd

--- pyLOM/vmmath/__init__.py
+++ pyLOM/vmmath/__init__.py
@@ -9,7 +9,7 @@
 __VERSION__ = '1.0.0'
 
 # Vector matrix routines
-from .wrapper import transpose, vector_norm, matmul, vecmat, eigen, RMSE
+from .wrapper import transpose, vector_norm, matmul, vecmat, eigen, RMSE, polar, diag, vandermonde
 # Averaging routines
 from .wrapper import temporal_mean, subtract_mean
 # SVD routines
