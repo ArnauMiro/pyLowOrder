b66301d829ba095fa986195113593ece599c859c
Benet Eiximeno Franch <beneteiximenofranch@hotmail.com>
2022-02-17 13:01:48 +0100

    Includes new functions

--- pyLOM/DMD/__init__.py
+++ pyLOM/DMD/__init__.py
@@ -9,12 +9,13 @@
 __VERSION__ = '1.0.0'
 
 # Functions coming from POD
-from ..POD.wrapper import temporal_mean, subtract_mean, truncate, residual, RMSE
+#from ..POD.wrapper import temporal_mean, subtract_mean, truncate, residual, RMSE
 #from .wrapper import run, truncate, PSD, reconstruct
 #from .wrapper import temporal_mean, subtract_mean, svd, residual, power_spectral_density, RMSE
 
 # Functions coming from DMD
-from .wrapper import svd, eigen, matrix_split, project_POD_basis, build_complex_eigenvectors
-from .wrapper import frequency_damping, mode_computation, amplitude_jovanovic, order_modes, vandermonde
+from .wrapper import frequency_damping, mode_computation, amplitude_jovanovic
+#from .wrapper import svd, eigen, matrix_split, project_POD_basis, build_complex_eigenvectors
+#from .wrapper import frequency_damping, mode_computation, amplitude_jovanovic, order_modes, vandermonde
 
-del wrapper
+#del wrapper
