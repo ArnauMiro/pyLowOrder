005d9d2e3b8910a1a15a1564012e4ebabfeba3d4
Benet Eiximeno Franch <beneteiximenofranch@hotmail.com>
2022-02-17 12:59:48 +0100

    more cases

--- Examples/example_cholesky.py
+++ Examples/example_cholesky.py
@@ -1,16 +1,17 @@
 import numpy as np
-import pyLOM
 from scipy.linalg import ldl
 
 #Test matrix
 A = np.array([
-	[1.0000,0.5000,0.3333,0.2500],
-    [0.5000,1.0000,0.6667,0.5000],
-    [0.3333,0.6667,1.0000,0.7500],
-    [0.2500,0.5000,0.7500,-1.000]])
+	[1, 0, 1],
+    [0, 2, 0],
+    [1, 0, 3]])
+print('A: \n', A)
 
 #Cholesky decomposition from NumPy
-try:
-	print(np.linalg.cholesky(A))
-except:
-	print(ldl(A))
+Achol = np.linalg.cholesky(A)
+print('Cholesky: \n', Achol)
+
+#LDL decomposition from scipy
+Aldl, a, b = ldl(A)
+print('LDL: \n', Aldl)
