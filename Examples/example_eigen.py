ecaa8ae67b43e008a78598d3f44350472ecdb3c7
Benet Eiximeno Franch <beneteiximenofranch@hotmail.com>
2022-02-17 13:00:45 +0100

    changes

--- Examples/example_eigen.py
+++ Examples/example_eigen.py
@@ -10,15 +10,15 @@ import numpy as np
 import pyLOM
 
 
-## Define matrix A 4x4
+## Example 1
 A = np.array([
 	[1.0000,0.5000,0.3333,0.2500],
     [0.5000,1.0000,0.6667,0.5000],
     [0.3333,0.6667,1.0000,0.7500],
     [0.2500,0.5000,0.7500,1.0000]],dtype=np.double,order='C')
+print('A:\n',A)
 
-
-## Run eigenvalues from numpy
+# Run eigenvalues from numpy
 pyLOM.cr_start('eigen numpy',0)
 w,v = np.linalg.eig(A)
 pyLOM.cr_stop('eigen numpy',0)
@@ -26,8 +26,25 @@ print('Numpy:')
 print(w.shape,w)
 print(v.shape,v)
 
+# Run eigenvalyes from DMD
+delta,w,v = pyLOM.math.eigen(A)
+print('pyLOM:')
+print(delta.shape,delta)
+print(w.shape,w)
+print(v.shape,v)
 
-## Run eigenvalyes from DMD
+
+## Example 2
+A = np.array([[5, 4, 3, 2], [8, 6, 2, 1], [9, 4, 3, 6], [7, 5, 3, 0]],dtype=np.double)
+print('A:\n',A)
+
+# Run eigenvalues from numpy
+w,v = np.linalg.eig(A)
+print('Numpy:')
+print(w.shape,w)
+print(v.shape,v)
+
+# Run eigenvalyes from DMD
 delta,w,v = pyLOM.math.eigen(A)
 print('pyLOM:')
 print(delta.shape,delta)
