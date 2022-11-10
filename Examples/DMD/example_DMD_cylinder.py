196f1b1749a773018d8e39becb43057ec87cf9dd
bef-18 <beneteiximenofranch@hotmail.com>
2022-11-10 17:31:49 +0100

    More modes are plotted

--- Examples/DMD/example_DMD_cylinder.py
+++ Examples/DMD/example_DMD_cylinder.py
@@ -18,7 +18,7 @@ VARIABLE = 'VELOX'
 X  = d[VARIABLE]
 dt = 0.2
 remove_mean = False
-r = 20
+r = 1e-6
 pyLOM.cr_start('example',0)
 t = np.arange(0, 150, dtype = np.double)
 
@@ -30,8 +30,9 @@ muReal, muImag, Phi, bJov = pyLOM.DMD.run(Y, r, remove_mean)
 delta, omega = pyLOM.DMD.frequency_damping(muReal, muImag, dt)
 
 #Reconstruction according to Jovanovic 2014
+X2 = X[:, :-1].copy()
 X_DMD = pyLOM.DMD.reconstruction_jovanovic(Phi, muReal, muImag, t, bJov)
-rmse = pyLOM.math.RMSE(X_DMD.real, X[:, :-1])
+rmse = pyLOM.math.RMSE(X_DMD.copy(), X2)
 print('RMSE = %e' % rmse)
 
 #Ritz Spectrum
@@ -44,10 +45,10 @@ pyLOM.DMD.amplitudeFrequency(omega, bJov, norm = False)
 pyLOM.DMD.dampingFrequency(omega, delta)
 
 #Plot modes and reconstructed flow
-pyLOM.DMD.plotMode(Phi, muImag/dt, d.xyz, d.mesh, d.info(VARIABLE), modes = [1, 2, 3]) #MATLAB: modes 17, 112 and 144
-fig,ax,anim = pyLOM.DMD.animateFlow(X,X_DMD.real,d.xyz,d.mesh,d.info(VARIABLE),dim=0)
+pyLOM.DMD.plotMode(Phi, muImag/dt, d.xyz, d.mesh, d.info(VARIABLE), modes = [1, 2, 3, 4, 5, 6, 7]) #MATLAB: modes 17, 112 and 144
+fig,ax,anim = pyLOM.DMD.animateFlow(X,X_DMD,d.xyz,d.mesh,d.info(VARIABLE),dim=0)
 
 ## Show and print timings
 pyLOM.cr_stop('example',0)
 pyLOM.cr_info()
-pyLOM.show_plots()
+#pyLOM.show_plots()
