348a7704d5fe0b3f144169ec176e268ad57eb2da
bef-18 <beneteiximenofranch@hotmail.com>
2022-11-22 09:52:22 +0100

    Merge of the serial and parallel examples of the RMSE

--- Examples/example_RMSE.py
+++ Examples/example_RMSE.py
@@ -8,11 +8,14 @@ from __future__ import print_function, division
 
 import numpy as np
 import pyLOM
+from pyLOM.utils.parall import mpi_scatter
 
 ## Define matrices to compare
 A  = np.array([[1,2],[3,4],[5,6],[7,8]],dtype=np.double,order='C')
+Ai = mpi_scatter(A,root=0,do_split=True)
 B  = np.array([[1,6],[3,4],[10,6],[7,8]],dtype=np.double,order='C')
+Bi = mpi_scatter(B,root=0,do_split=True)
 
 ## Compute RMSE
 r = pyLOM.math.RMSE(A, B)
-print('RMSE is: ', r)
+pyLOM.pprint(0, 'RMSE is: ', r, flush = True)
