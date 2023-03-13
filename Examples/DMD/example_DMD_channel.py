cf7db4f1174bc71a389bd55ef8ad4fae8c42fad5
Arnau Miro <arnau.miro@upc.edu>
2023-03-13 15:51:20 +0100

    Fixing examples

--- Examples/DMD/example_DMD_channel.py
+++ Examples/DMD/example_DMD_channel.py
@@ -14,13 +14,12 @@ DATAFILE  = '../channel.h5'
 VARIABLES = ['VELOC']
 
 d  = pyLOM.Dataset.load(DATAFILE)
-X  = d.X(*VARIABLES)[0:3*mesh.npoints:3,:] # Select only X component
-dt = t[1] - t[0]
+X  = d.X(*VARIABLES)[0:3*d.mesh.npoints:3,:] # Select only X component
+dt = d.time[1] - d.time[0]
 
 
 ## Run DMD
-Y = X[:,:100].copy()
-muReal,muImag,Phi,bJov = pyLOM.DMD.run(Y,1e-6,remove_mean=False)
+muReal,muImag,Phi,bJov = pyLOM.DMD.run(X,1e-6,remove_mean=False)
 # Compute frequency and damping ratio of the modes
 delta, omega = pyLOM.DMD.frequency_damping(muReal,muImag,dt)
 pyLOM.DMD.save('results.h5',muReal,muImag,Phi,bJov,d.partition_table,nvars=1,pointData=True)
@@ -35,9 +34,10 @@ if pyLOM.utils.is_rank_or_serial(0):
 	fig.savefig('dampingFrequency.png',dpi=300)
 
 # Spatial modes
-d.add_variable('U_MODES_REAL',True,Phi.shape[1],pyLOM.DMD.extract_modes(Phi,1,d.mesh.npoints,real=True))
-d.add_variable('U_MODES_IMAG',True,Phi.shape[1],pyLOM.DMD.extract_modes(Phi,1,d.mesh.npoints,real=False))
-d.write('modes',basedir='modes',instants=[0],times=[0.],vars=['P_MODES_REAL','P_MODES_IMAG'],fmt='vtkh5')
+modes = np.arange(1,100+1,dtype=np.int32)
+d.add_variable('U_MODES_REAL',True,len(modes),pyLOM.DMD.extract_modes(Phi,1,d.mesh.npoints,modes=modes,real=True))
+d.add_variable('U_MODES_IMAG',True,len(modes),pyLOM.DMD.extract_modes(Phi,1,d.mesh.npoints,modes=modes,real=False))
+d.write('modes',basedir='modes',instants=[0],times=[0.],vars=['U_MODES_REAL','U_MODES_IMAG'],fmt='vtkh5')
 
 
 ## Prediction
