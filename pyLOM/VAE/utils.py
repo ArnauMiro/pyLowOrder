fad8031340342419c20bd29de2e2f7e9e7cd014d
bef-18 <beneteiximenofranch@hotmail.com>
2023-11-07 13:27:02 +0100

    add multichannel dataset

--- pyLOM/VAE/utils.py
+++ pyLOM/VAE/utils.py
@@ -76,3 +76,64 @@ class Dataset(torch_dataset):
         train_loader = torch.utils.data.DataLoader(train, batch_size=batch_size, shuffle=True)
         vali_loader  = torch.utils.data.DataLoader(vali, batch_size=batch_size, shuffle=True)
         return train_loader, vali_loader
+
+class MultiChannelDataset(torch_dataset):
+    def __init__(self, vars, nx, ny, time, transform=None):
+        self._data = self._normalize(vars)
+        self._nx = nx
+        self._ny = ny
+        self._time = time
+        self.transform = transform
+        self._n_channels = len(vars)
+
+    def __len__(self):
+        return len(self._time)
+
+    def __getitem__(self, index):
+        snap = torch.cat([torch.Tensor(self._data[i][:, index]).view(1, self._nx, self._ny) for i in range(self._n_channels)], dim=0)
+        return snap
+
+    @property
+    def nx(self):
+        return self._nx
+
+    @property
+    def ny(self):
+        return self._ny
+
+    @property
+    def data(self):
+        return self._data
+
+    @property
+    def time(self):
+        return self._time
+
+    @property
+    def n_channels(self):
+        return self._n_channels
+
+    def _normalize(self, data_tuple):
+        normalized_data = []
+        for i in range(self._n_channels):
+            data = data_tuple[i]
+            mean = data.mean()
+            fluc = data - mean
+            maxi = fluc.max()
+            normalized_data.append(fluc / maxi)
+        return normalized_data
+
+    def recover(self, data):
+        recovered_data = []
+        for i in range(self._n_channels):
+            recovered_data.append(data[i] * data[i].max() + data[i].mean())
+        return recovered_data
+
+    def split(self, ptrain, pvali, batch_size=1):
+        len_train = int(ptrain * len(self))
+        len_vali = int(pvali * len(self))
+        len_train = len_train + len(self) - (len_train + len_vali)
+        train, vali = torch.utils.data.random_split(self, (len_train, len_vali))
+        train_loader = torch.utils.data.DataLoader(train, batch_size=batch_size, shuffle=True)
+        vali_loader = torch.utils.data.DataLoader(vali, batch_size=batch_size, shuffle=True)
+        return train_loader, vali_loader
