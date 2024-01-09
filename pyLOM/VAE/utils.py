6ee22d5425d9da669a97bfc0e5cbac33d6fe45c4
bef-18 <beneteiximenofranch@hotmail.com>
2024-01-09 14:46:36 +0100

    Adding a dataset for denoising images

--- pyLOM/VAE/utils.py
+++ pyLOM/VAE/utils.py
@@ -16,66 +16,35 @@ def create_results_folder(RESUDIR):
 def select_device():
     return torch.device("cuda" if torch.cuda.is_available() else "cpu") 
 
-class Dataset(torch_dataset):
-    @cr("Init VAE dataset")
-    def __init__(self, var, nx, ny, time, transform = None):
-        self._data, self._mean, self._max = self._normalize(var)
-        self._nx                          = np.int(nx)
-        self._ny                          = np.int(ny)
-        self._time                        = time
-        self.transform                    = transform
-        
+class DenoisingDataset(torch_dataset):
+    def __init__(self, input, target, nx, ny, transform = None):
+        self._input  = input
+        self._target = target
+        self._nx     = nx
+        self._ny     = ny
+        self.transform = transform
+
     def __len__(self):
-        return len(self.time)
-    
+        return len(self._input[0,:])
+          
     def __getitem__(self, index):
-        snap = torch.Tensor(self.data[:,index])
-        snap = snap.resize_(1,self.nx,self.ny)        
-        return snap
+        input  = torch.Tensor(self._input[:,index])
+        input  = input.resize_(1,self.nx,self.ny) 
+        target = torch.Tensor(self._target[:,index])
+        target = target.resize_(1,self.nx,self.ny)         
+        return input, target
     
     @property
     def nx(self):
         return self._nx
-    
+
     @property
     def ny(self):
         return self._ny
-    
-    @property
-    def data(self):
-        return self._data
-    
-    @property
-    def mean(self):
-        return self._mean
-    
-    @property
-    def max(self):
-        return self._max
-    
-    @property
-    def time(self):
-        return self._time
-
-    def _normalize(self, var):
-        mean = temporal_mean(var)
-        fluc = subtract_mean(var, mean)
-        maxi = fluc.max()
-        return np.array(fluc/fluc.max()), mean, maxi
-    
-    def recover(self, var):
-        return var*self.max + np.tile(self.mean,(var.shape[1],1)).T
-    
-    def split(self, ptrain, pvali, batch_size=1):
-        #Compute number of snapshots
-        len_train = int(ptrain*len(self))
-        len_vali  = int(pvali*len(self))
-        len_train = len_train + len(self) - (len_train + len_vali)
-        #Select data
-        train, vali  = torch.utils.data.random_split(self,(len_train,len_vali))
-        train_loader = torch.utils.data.DataLoader(train, batch_size=batch_size, shuffle=True)
-        vali_loader  = torch.utils.data.DataLoader(vali, batch_size=batch_size, shuffle=True)
-        return train_loader, vali_loader
+       
+    def loader(self, batch_size=1):
+        loader  = torch.utils.data.DataLoader(dataset=self, batch_size=batch_size, shuffle=True)
+        return loader
 
 class MultiChannelDataset(torch_dataset):
     def __init__(self, vars, nx, ny, time, device='cpu', transform=None):
@@ -126,26 +95,46 @@ class MultiChannelDataset(torch_dataset):
         maxi = np.zeros((self._n_channels,),dtype=float)
         for ichan in range(self._n_channels):
             var           = vars[ichan]
-            mean[ichan,:] = temporal_mean(var)
-            ifluc         = subtract_mean(var,mean[ichan,:])
-            maxi[ichan]   = ifluc.max()
+            #mean[ichan,:] = temporal_mean(var)
+            ifluc         = -subtract_mean(var,mean[ichan,:])
+            maxi[ichan]   = np.max(np.abs(var))#-ifluc.max()
             data.append(var)#ifluc/maxi[ichan])
         return data, mean, maxi
 
     def recover(self, data):
         recovered_data = []
         for i in range(self._n_channels):
-            recovered_data.append(data[i])# * data[i].max() + data[i].mean())
+            recovered_data.append(data[i])# + data[i].mean())
         return recovered_data
 
-    def split(self, ptrain, pvali, batch_size=1):
+    def loader(self, batch_size=1):
         #Compute number of snapshots
-        len_train = int(ptrain*len(self))
-        len_vali  = int(pvali*len(self))
-        len_train = len_train + len(self) - (len_train + len_vali)
-        #Select data
-        train, vali  = torch.utils.data.random_split(self,(len_train,len_vali))
-        train_loader = torch.utils.data.DataLoader(train, batch_size=batch_size, shuffle=True)
-        vali_loader  = torch.utils.data.DataLoader(vali, batch_size=batch_size, shuffle=True)
+        loader = torch.utils.data.DataLoader(self, batch_size=batch_size, shuffle=True)
+        return loader
     
+    def split_subdatasets(self, ptrain, pvali, batch_size=1, subdatasets=1):
+        ##Compute number of snapshots
+        total_len = len(self)
+        sub_len   = total_len // subdatasets
+        len_train = int(ptrain*sub_len)
+        len_vali  = int(pvali*sub_len)
+        len_train = len_train + sub_len - (len_train + len_vali)
+        
+        ##Select data
+        # Initialize lists to store subsets
+        train_subsets = []
+        vali_subsets  = []
+        # Split each third into training and validation
+        for i in range(0, total_len, sub_len):
+            sub_dataset = torch.utils.data.Subset(self, range(i, i + sub_len))
+            train_subset, vali_subset = torch.utils.data.random_split(sub_dataset, (len_train, len_vali))
+            train_subsets.append(train_subset)
+            vali_subsets.append(vali_subset)
+        # Combine subsets from each third
+        combined_train_dataset = torch.utils.data.ConcatDataset(train_subsets)
+        combined_vali_dataset = torch.utils.data.ConcatDataset(vali_subsets)
+        # Create DataLoaders
+        train_loader = torch.utils.data.DataLoader(combined_train_dataset, batch_size=batch_size, shuffle=True)
+        vali_loader  = torch.utils.data.DataLoader(combined_vali_dataset, batch_size=batch_size, shuffle=True)
+
         return train_loader, vali_loader
