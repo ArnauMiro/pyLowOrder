8f822ddabee8aa40c460692fa91abe1c0625d5ed
Arnau Miro <arnau.miro@upc.edu>
2024-09-20 07:54:31 +0200

    Modified SPOD examples

--- Examples/SPOD/example_SPOD_channel.py
+++ Examples/SPOD/example_SPOD_channel.py
@@ -8,24 +8,24 @@ from __future__ import print_function, division
 import mpi4py
 mpi4py.rc.recv_mprobe = False
 
-import os, numpy as np
+import numpy as np
 import pyLOM
 
 
 ## Data loading
-DATAFILE  = '../channel.h5'
+DATAFILE  = './channel.h5'
 VARIABLES = ['PRESS','VELOX','VELOY','VELOZ']
 
-d     = pyLOM.Dataset.load(DATAFILE)
+m     = pyLOM.Mesh.load(DATAFILE)
+d     = pyLOM.Dataset.load(DATAFILE,ptable=m.partition_table)
 X     = d.X(*VARIABLES)
-t     = d.time
 npwin = 100 # Number of snapshots in each window
 nolap = 20  # Number of overlapping snapshots between windows
 
 
 ## Run SPOD
 L, P, f = pyLOM.SPOD.run(X,t,nDFT=npwin,nolap=nolap,remove_mean=True)
-pyLOM.SPOD.save('results.h5',L,P,f,d.partition_table,nvars=4,pointData=True)
+pyLOM.SPOD.save('results.h5',L,P,f,d.partition_table,nvars=4,pointData=d.point)
 if pyLOM.utils.is_rank_or_serial(root=0): 
 	fig,_ = pyLOM.SPOD.plotSpectra(f, L)
 	fig.savefig('spectra.png',dpi=300)
@@ -34,11 +34,11 @@ if pyLOM.utils.is_rank_or_serial(root=0):
 ## Dump to ParaView
 # Spatial modes
 modes = np.arange(1,10+1,dtype=np.int32)
-d.add_variable('spatial_modes_P',True,len(modes),pyLOM.SPOD.extract_modes(L,P,1,d.mesh.npoints,modes=modes))
-d.add_variable('spatial_modes_U',True,len(modes),pyLOM.SPOD.extract_modes(L,P,2,d.mesh.npoints,modes=modes))
-d.add_variable('spatial_modes_V',True,len(modes),pyLOM.SPOD.extract_modes(L,P,3,d.mesh.npoints,modes=modes))
-d.add_variable('spatial_modes_W',True,len(modes),pyLOM.SPOD.extract_modes(L,P,4,d.mesh.npoints,modes=modes))
-d.write('modes',basedir='modes',instants=[0],times=[0.],vars=['spatial_modes_P','spatial_modes_U','spatial_modes_V','spatial_modes_W'],fmt='vtkh5')
+d.add_field('spatial_modes_P',len(modes),pyLOM.SPOD.extract_modes(L,P,1,len(d),modes=modes))
+d.add_field('spatial_modes_U',len(modes),pyLOM.SPOD.extract_modes(L,P,2,len(d),modes=modes))
+d.add_field('spatial_modes_V',len(modes),pyLOM.SPOD.extract_modes(L,P,3,len(d),modes=modes))
+d.add_field('spatial_modes_W',len(modes),pyLOM.SPOD.extract_modes(L,P,4,len(d),modes=modes))
+pyLOM.io.pv_writer(m,d,'modes',basedir='modes',instants=[0],times=[0.],vars=['spatial_modes_P','spatial_modes_U','spatial_modes_V','spatial_modes_W'],fmt='vtkh5')
 
 
 ## Show and print timings
--- Examples/SPOD/example_SPOD_jet.py
+++ Examples/SPOD/example_SPOD_jet.py
@@ -8,34 +8,34 @@ from __future__ import print_function, division
 import mpi4py
 mpi4py.rc.recv_mprobe = False
 
-import os, numpy as np
 import pyLOM
 
 
 ## Parameters
-DATAFILE = 'Examples/Data/jetLES.h5'
+DATAFILE = './DATA/jetLES.h5'
 VARIABLE = 'PRESS'
 
 
 ## Data loading
-d     = pyLOM.Dataset.load(DATAFILE)
-X     = d[VARIABLE][:,::10]
-t     = d.time[::10]
+m     = pyLOM.Mesh.load(DATAFILE)
+d     = pyLOM.Dataset.load(DATAFILE,ptable=m.partition_table)
+X     = d[VARIABLE][:,::10].copy()
+t     = d.get_variable('time')[::10].copy()
 npwin = 150 #Number of snapshots in each window
 nolap = 50 #Number of overlapping snapshots between windows
 
 
 ## Run SPOD
 L, P, f = pyLOM.SPOD.run(X,t,nDFT=npwin,nolap=nolap,remove_mean=True)
-pyLOM.SPOD.save('results.h5',L,P,f,d.partition_table,nvars=1,pointData=True)
+pyLOM.SPOD.save('results.h5',L,P,f,d.partition_table,nvars=1,pointData=d.point)
 if pyLOM.utils.is_rank_or_serial(root=0): pyLOM.SPOD.plotSpectra(f, L)
 
 
 ## Dump to ParaView
 # Spatial modes
-d.add_variable('spatial_modes_U',True,6,pyLOM.SPOD.extract_modes(L,P,1,d.mesh.npoints,modes=[1,2,3,4,5,6]))
-d.write('modes',basedir='out/modes',instants=[0],times=[0.],vars=['spatial_modes_U'],fmt='vtkh5')
-pyLOM.SPOD.plotMode(L,P,f,d,1,pointData=True,modes=[1,2,3,4,5,6],cpos='xy')
+d.add_field('spatial_modes_U',6,pyLOM.SPOD.extract_modes(L,P,1,len(d),modes=[1,2,3,4,5,6]))
+pyLOM.io.pv_writer(m,d,'modes',basedir='out/modes',instants=[0],times=[0.],vars=['spatial_modes_U'],fmt='vtkh5')
+pyLOM.SPOD.plotMode(L,P,f,m,d,1,pointData=d.point,modes=[1,2,3,4,5,6],cpos='xy')
 
 
 ## Show and print timings
