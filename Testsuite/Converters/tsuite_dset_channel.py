e48c36f87adb2f330449c317e57cba1d5f30177e
Arnau Miro <arnau.miro@upc.edu>
2024-09-24 10:44:22 +0200

    Adding VAE to testsuite

--- Testsuite/Converters/tsuite_dset_channel.py
+++ Testsuite/Converters/tsuite_dset_channel.py
@@ -6,7 +6,7 @@
 # Last revision: 03/08/2021
 from __future__ import print_function, division
 
-import os, numpy as np
+import numpy as np
 import pyAlya, pyLOM
 
 
@@ -14,7 +14,7 @@ BASEDIR        = '../'
 DSETDIR        = './'
 CASESTR        = 'channel'
 VARLIST        = ['VELOC','PRESS']
-START, DT, END = 500000, 1000, 505000
+START, DT, END = 500000, 500, 505000
 
 # In case of restart, load the previous data
 listOfInstants = [ii for ii in range(START,END,DT)]
@@ -29,20 +29,20 @@ pyAlya.pprint(0,'Run (%d instants)...' % len(listOfInstants),flush=True)
 ## Create POD dataset
 m = pyLOM.Mesh.from_pyAlya(mesh)
 p = pyLOM.PartitionTable.from_pyAlya(mesh.partition_table,has_master=True)
-d = pyLOM.Dataset(ptable=p, mesh=m, time=np.zeros((ni,),dtype=np.double))
 
 
 ## Build dataset from the instants
+time    = np.zeros((len(listOfInstants),),np.double)
 X_PRESS = np.zeros((mesh.nnod,ni),dtype=np.double) # POD matrix, VELOC and PRESS
 X_VELOX = np.zeros((mesh.nnod,ni),dtype=np.double) # POD matrix, VELOC and PRESS
 X_VELOY = np.zeros((mesh.nnod,ni),dtype=np.double) # POD matrix, VELOC and PRESS
 X_VELOZ = np.zeros((mesh.nnod,ni),dtype=np.double) # POD matrix, VELOC and PRESS
 for ii,instant in enumerate(listOfInstants):
-	if ii%10 == 0: pyAlya.pprint(1,'Processing instant %d...'%instant,flush=True)
+	if ii%100 == 0: pyAlya.pprint(1,'Processing instant %d...'%instant,flush=True)
 	# Read data
 	field, header = pyAlya.Field.read(CASESTR,VARLIST,instant,mesh.xyz,basedir=BASEDIR)
 	# Store time
-	d.time[ii] = header.time
+	time[ii] = header.time
 	# Store the POD matrix
 	X_PRESS[:,ii] = field['PRESS']
 	X_VELOX[:,ii] = field['VELOC'][:,0]
@@ -50,15 +50,26 @@ for ii,instant in enumerate(listOfInstants):
 	X_VELOZ[:,ii] = field['VELOC'][:,2]
 
 
-## Add variables to the dataset
-#d.add_variable('PRESS',True,1,X_PRESS)
-d.add_variable('VELOX',True,1,X_VELOX)
-d.add_variable('VELOY',True,1,X_VELOY)
-d.add_variable('VELOZ',True,1,X_VELOZ)
+## Create dataset for pyLOM
+d = pyLOM.Dataset(xyz=m.xyz, ptable=p, order=m.pointOrder, point=True,
+	# Add the time as the only variable
+	vars  = {'time':{'idim':0,'value':time}},
+	# Now add all the arrays to be stored in the dataset
+	# It is important to convert them as C contiguous arrays
+	PRESS = {'ndim':1,'value':X_PRESS},
+	VELOX = {'ndim':1,'value':X_VELOX},
+	VELOY = {'ndim':1,'value':X_VELOY},
+	VELOZ = {'ndim':1,'value':X_VELOZ},
+)
 
 
 ## Store dataset
-d.save('CHANNEL.h5',nopartition=True) 
+# Nopartition will eliminate the partition of alya and allow the dataset to be run
+# with any number of processors but will increase the save time to the disk
+# Use with great care!!
+m.save('%s.h5'%CASESTR,nopartition=True) 
+d.save('%s.h5'%CASESTR,nopartition=True) 
+
 
 pyAlya.cr_info()
 pyLOM.cr_info()
---
+++ Testsuite/runs/tsuite_VAE2D.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python
+#
+# PYLOM Testsuite 2D-VAE.
+#
+# Last revision: 24/09/2024
+from __future__ import print_function, division
+
+import mpi4py
+mpi4py.rc.recv_mprobe = False
+
+import sys, os, numpy as np
+import pyLOM
+
+DATAFILE = sys.argv[1]
+VARIABLE = sys.argv[2]
+OUTDIR   = sys.argv[3]
+
+
+## Set device
+device = pyLOM.NN.select_device()
+
+
+## Specify autoencoder parameters
+ptrain      = 0.8
+pvali       = 0.2
+batch_size  = 1
+nepochs     = 10
+nlayers     = 1
+channels    = 32
+lat_dim     = 10
+beta        = 0
+kernel_size = 4
+nlinear     = 256
+padding     = 1
+activations = [pyLOM.NN.tanh(), pyLOM.NN.tanh(), pyLOM.NN.tanh(), pyLOM.NN.tanh(), pyLOM.NN.tanh(), pyLOM.NN.tanh(), pyLOM.NN.tanh()]
+batch_norm  = True
+
+
+## Load pyLOM dataset and set up results output
+RESUDIR = os.path.join(OUTDIR,'vae_beta_%.2e_ld_%i' % (beta, lat_dim))
+pyLOM.NN.create_results_folder(RESUDIR)
+
+
+## Mesh size (HARDCODED BUT MUST BE INCLUDED IN PYLOM DATASET)
+n0h = 449
+n0w = 199
+nh  = 448
+nw  = 192
+
+
+## Create a torch dataset
+m    = pyLOM.Mesh.load(DATAFILE)
+d    = pyLOM.Dataset.load(DATAFILE,ptable=m.partition_table)
+u_x  = d[VARIABLE]
+time = d.get_variable('time')
+td   = pyLOM.NN.Dataset((u_x,), (n0h, n0w), time, transform=False)
+td.data[0] = np.transpose(np.array([td.data[0][:,0]]))
+td._time   = np.array([td.time[0]])
+td.crop((nh, nw), (n0h, n0w))
+trloader = td.loader()
+
+
+## Set and train the variational autoencoder
+encarch    = pyLOM.NN.Encoder2D(nlayers, lat_dim, nh, nw, td.n_channels, channels, kernel_size, padding, activations, nlinear, batch_norm=batch_norm)
+decarch    = pyLOM.NN.Decoder2D(nlayers, lat_dim, nh, nw, td.n_channels, channels, kernel_size, padding, activations, nlinear, batch_norm=batch_norm)
+ae         = pyLOM.NN.Autoencoder(lat_dim, (nh, nw), td.n_channels, encarch, decarch, device=device)
+early_stop = pyLOM.NN.EarlyStopper(patience=5, min_delta=0.02)
+ae.train_model(trloader, trloader, nepochs, callback=early_stop, BASEDIR=RESUDIR)
+
+
+## Reconstruct dataset and compute accuracy
+rec = ae.reconstruct(td)
+rd  = pyLOM.NN.Dataset((rec), (nh, nw), td._time, transform=False)
+rd.pad((nh, nw), (n0h, n0w))
+td.pad((nh, nw), (n0h, n0w))
+d.add_field('urec',1,rd.data[0][:,0].numpy())
+d.add_field('utra',1,td.data[0][:,0])
+pyLOM.io.pv_writer(m,d,'reco',basedir=RESUDIR,instants=np.arange(time.shape[0],dtype=np.int32),times=time,vars=['urec', VARIABLE, 'utra'],fmt='vtkh5')
+pyLOM.NN.plotSnapshot(m,d,vars=['urec'],instant=0,component=0,cmap='jet',cpos='xy')
+pyLOM.NN.plotSnapshot(m,d,vars=['utra'],instant=0,component=0,cmap='jet',cpos='xy')
+
+
+## Testsuite output
+pyLOM.pprint(0,'TSUITE u_x  =',u_x.min(),u_x.max(),u_x.mean())
+pyLOM.pprint(0,'TSUITE urec =',d['urec'].min(),d['urec'].max(),d['urec'].mean())
+pyLOM.pprint(0,'TSUITE utra =',d['utra'].min(),d['utra'].max(),d['utra'].mean())
+
+
+pyLOM.cr_info()
+pyLOM.pprint(0,'End of output')
