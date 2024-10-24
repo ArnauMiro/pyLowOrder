72673eb4e7a14309237734214c8c03667a6936a8
Arnau Miro <arnau.miro@upc.edu>
2024-10-24 10:45:43 +0200

    Fixing AE

--- Testsuite/runs/tsuite_AE2D.py
+++ Testsuite/runs/tsuite_AE2D.py
@@ -48,7 +48,9 @@ nw  = 192
 
 
 ## Create a torch dataset
-u_x  = d.X(*VARIABLES)[:,0]
+u    = d.X(*VARIABLES)
+um   = pyLOM.math.temporal_mean(u)
+u_x  = pyLOM.math.subtract_mean(u, um)[:,0]
 td   = pyLOM.NN.Dataset((u_x,), (n0h, n0w))
 td.crop(nh, nw)
 
@@ -64,7 +66,7 @@ pipeline = pyLOM.NN.Pipeline(
    test_dataset  = td,
    model         = model,
    training_params = {
-       "batch_size": 1,
+       "batch_size": 16,
        "epochs": 100,
        "lr": 1e-4,
        "callback":early_stop,
