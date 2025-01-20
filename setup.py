0853d4cb7dcd106b622b6e0887199a2886827969
Arnau Miro <arnau.miro@upc.edu>
2025-01-20 12:48:30 +0100

    Upgraded version

--- setup.py
+++ setup.py
@@ -196,7 +196,7 @@ modules_list = [
 ## Main setup
 setup(
 	name             = 'pyLowOrder',
-	version          = '2.0.1',
+	version          = '2.1.0',
 	author           = 'Benet Eiximeno, Beka Begiashvili, Arnau Miro, Eusebio Valero, Oriol Lehmkuhl',
 	author_email     = 'benet.eiximeno@bsc.es, beka.begiashvili@alumnos.upm.es, arnau.mirojane@bsc.es, eusebio.valero@upm.es, oriol.lehmkuhl@bsc.es',
 	maintainer       = 'Benet Eiximeno, Arnau Miro',
