c537d58f4fcba8698fdf06a4db23ba8c342f16f6
Isaac <fluidmechanicsinta@gmail.com>
2025-02-11 12:41:11 +0100

    Preliminary implementation of Isomap and MDS

---
+++ Examples/MANIFOLD/example_isomap.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#
+# Isomap analysis.
+#
+# Last revision: 11/02/2025
+
+
+import os, numpy as np
+import pyLOM
+from pyLOM.MANIFOLD.wrapper import isomap
+import matplotlib.pyplot as plt
+
+## Parameters
+DATAFILE = './Testsuite/DATA/CYLINDER.h5'
+VARIABLE = 'VELOC'
+
+
+## Data loading
+m = pyLOM.Mesh.load(DATAFILE)
+d = pyLOM.Dataset.load(DATAFILE,ptable=m.partition_table)
+X = d[VARIABLE]
+t = d.get_variable('time')
+
+Y,_,_ = isomap(X.T,2,6)
+
+plt.scatter(Y[0,:],Y[1,:],s=10,c='b')
+plt.show()
---
+++ Examples/MANIFOLD/example_mds.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#
+# Isomap analysis.
+#
+# Last revision: 11/02/2025
+
+
+import os, numpy as np
+import pyLOM
+from pyLOM.MANIFOLD.wrapper import mds
+import matplotlib.pyplot as plt
+
+## Parameters
+DATAFILE = './Testsuite/DATA/CYLINDER.h5'
+VARIABLE = 'VELOC'
+
+
+## Data loading
+m = pyLOM.Mesh.load(DATAFILE)
+d = pyLOM.Dataset.load(DATAFILE,ptable=m.partition_table)
+X = d[VARIABLE]
+t = d.get_variable('time')
+
+Y = mds(X.T,2)
+
+plt.scatter(Y[0,:],Y[1,:],s=10,c='b')
+plt.show()
