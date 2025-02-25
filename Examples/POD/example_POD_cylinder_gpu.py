548ccffd10a25c41bb4e9a99827090fa2b609812
Arnau Miro <arnau.miro@upc.edu>
2025-02-25 11:52:26 +0100

    Separated specific example

--- Examples/POD/example_POD_cylinder_gpu.py
+++ Examples/POD/example_POD_cylinder_gpu.py
@@ -9,8 +9,10 @@ import mpi4py
 mpi4py.rc.recv_mprobe = False
 
 import numpy as np
+import cupy  as cp
 import pyLOM
 
+pyLOM.gpu_device()
 
 ## Parameters
 DATAFILE = './DATA/CYLINDER.h5'
@@ -19,7 +21,7 @@ VARIABLE = 'VELOC'
 
 ## Data loading
 m = pyLOM.Mesh.load(DATAFILE)
-d = pyLOM.Dataset.load(DATAFILE,ptable=m.partition_table)
+d = pyLOM.Dataset.load(DATAFILE,ptable=m.partition_table).to_gpu([VARIABLE])
 X = d[VARIABLE]
 t = d.get_variable('time')
 
@@ -41,19 +43,20 @@ pyLOM.pprint(0,'RMSE = %e'%rmse)
 # Spatial modes
 d.add_field('spatial_modes_U',6,pyLOM.POD.extract_modes(PSI,1,len(d),modes=[1,4,6,2,5,3]))
 d.add_field('spatial_modes_V',6,pyLOM.POD.extract_modes(PSI,2,len(d),modes=[1,4,6,2,5,3]))
-pyLOM.io.pv_writer(m,d,'modes',basedir='out/modes',instants=[0],times=[0.],vars=['spatial_modes_U','spatial_modes_V'],fmt='vtkh5')
-pyLOM.POD.plotSnapshot(m,d,vars=['spatial_modes_U'],instant=0,component=0,cmap='jet',cpos='xy')
+pyLOM.io.pv_writer(m,d.to_cpu(['spatial_modes_U','spatial_modes_V']),'modes',basedir='out/modes',instants=[0],times=[0.],vars=['spatial_modes_U','spatial_modes_V'],fmt='vtkh5')
+pyLOM.POD.plotSnapshot(m,d.to_cpu(['spatial_modes_U']),vars=['spatial_modes_U'],instant=0,component=0,cmap='jet',cpos='xy')
 
 # Temporal evolution
 d.add_field('VELOR',2,X_POD)
-pyLOM.io.pv_writer(m,d,'flow',basedir='out/flow',instants=np.arange(t.shape[0],dtype=np.int32),times=t,vars=['VELOC','VELOR'],fmt='vtkh5')
-pyLOM.POD.plotSnapshot(m,d,vars=['VELOR'],instant=0,component=0,cmap='jet',cpos='xy')
+pyLOM.io.pv_writer(m,d.to_cpu(['VELOC','VELOR']),'flow',basedir='out/flow',instants=np.arange(t.shape[0],dtype=np.int32),times=t,vars=['VELOC','VELOR'],fmt='vtkh5')
+pyLOM.POD.plotSnapshot(m,d.to_cpu(['VELOR']),vars=['VELOR'],instant=0,component=0,cmap='jet',cpos='xy')
 
 
 ## Plot POD mode
 if pyLOM.utils.is_rank_or_serial(0):
 	# 0 - module, 1,2 - components
-	pyLOM.POD.plotMode(V[:,:-1],t[:-1],modes=[1,2,3,4])
+	pyLOM.POD.plotMode(V[:,:-1].get(),t[:-1],modes=[1,2,3,4])
+
 
 
 ## Show and print timings
