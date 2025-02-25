dad9da65df80e1702b3178c0d16a7b80b75601f1
Arnau Miro <arnau.miro@upc.edu>
2025-02-25 12:19:04 +0100

    Adding GPU examples of MANIFOLD

---
+++ Examples/MANIFOLD/example_mds_cylinder_gpu.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+#
+# MDS analysis.
+#
+# Last revision: 11/02/2025
+import pyLOM
+import matplotlib.pyplot as plt
+
+pyLOM.gpu_device()
+
+
+## Parameters
+DATAFILE = './DATA/CYLINDER.h5'
+VARIABLE = 'VELOC'
+
+## Data loading
+m = pyLOM.Mesh.load(DATAFILE)
+d = pyLOM.Dataset.load(DATAFILE,ptable=m.partition_table).to_gpu([VARIABLE])
+X = d[VARIABLE]
+t = d.get_variable('time')
+
+Y = pyLOM.MANIFOLD.mds(X,2)
+
+plt.scatter(Y[0,:],Y[1,:],s=10,c='b')
+
+pyLOM.cr_info()
+plt.show()
