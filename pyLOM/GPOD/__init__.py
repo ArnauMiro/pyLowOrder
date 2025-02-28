e618544a6f1ec10b0ce72ae2bbf07cf4e19009cd
Arnau Miro <arnau.miro@upc.edu>
2025-02-28 13:19:31 +0100

    Fix init

--- pyLOM/GPOD/__init__.py
+++ pyLOM/GPOD/__init__.py
@@ -2,16 +2,11 @@
 #
 # pyLOM - Python Low Order Modeling.
 #
-# POD Module
+# Gappy-POD Module
 #
-# Last rev: 09/07/2021
-
-__VERSION__ = "1.0.0"
+# Last rev: 28/02/2025
 
 from .wrapper import GappyPOD
 from .utils import set_random_elements_to_zero
 
-# from .plots import plotResidual, plotMode, plotSnapshot
-
-
-# del wrapper, plots
+del wrapper
