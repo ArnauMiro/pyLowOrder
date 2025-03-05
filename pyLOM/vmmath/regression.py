f1c1f3a3e158cd6dfc09ebd47b64e8d433db90ac
Arnau Miro <arnau.miro@upc.edu>
2025-03-05 11:56:17 +0100

    Adding regression

---
+++ pyLOM/vmmath/regression.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env cpython
+#
+# pyLOM - Python Low Order Modeling.
+#
+# Math operations module - regression.
+#
+# Last rev: 27/10/2021
+from __future__ import print_function, division
+
+import numpy as np
