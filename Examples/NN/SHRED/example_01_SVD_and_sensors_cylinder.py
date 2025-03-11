793fb2cc89f8f596875819af7028e53cade23936
bef-18 <beneteiximenofranch@hotmail.com>
2025-03-12 00:28:35 +0100

    paralell version

--- Examples/NN/SHRED/example_01_SVD_and_sensors_cylinder.py
+++ Examples/NN/SHRED/example_01_SVD_and_sensors_cylinder.py
@@ -18,17 +18,15 @@ VARIABLE = 'VELOX'
 ## Data loading
 m = pyLOM.Mesh.load(DATAFILE)
 d = pyLOM.Dataset.load(DATAFILE,ptable=m.partition_table)
-X = d[VARIABLE]
-t = d.get_variable('time')
 
 ## Extract sensors
 # Generate random sensors
-nsens  = 15 # Number of sensors
-x0, x1 = np.min(m.xyz[:,0]), np.max(m.xyz[:,0]) # Bounds at the X axis of the region where the sensor will be located
-y0, y1 = np.min(m.xyz[:,1]), np.max(m.xyz[:,1]) # Bounds at the Y axis of the region where the sensor will be located
+nsens  = 20 # Number of sensors
+x0, x1 = 0.5, 8 # Bounds at the X axis of the region where the sensor will be located
+y0, y1 = -1, 1 # Bounds at the Y axis of the region where the sensor will be located
 bounds = np.array([x0,x1,y0,y1])
 dsens  = d.select_random_sensors(nsens, bounds)
-dsens.save('sensors.h5')
+dsens.save('sensors.h5', nopartition=True)
 
 ## Run POD
 PSI,S,V = pyLOM.POD.run(X,remove_mean=True,randomized=True,r=8,q=3)
