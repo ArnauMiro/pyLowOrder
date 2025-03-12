86aada8c874bf7da780a63b35ebdc8ffeeae2e2c
bef-18 <beneteiximenofranch@hotmail.com>
2025-03-12 19:42:16 +0100

    works reading from pyLOM

--- Examples/NN/SHRED/example_02_shred_cylinder.py
+++ Examples/NN/SHRED/example_02_shred_cylinder.py
@@ -6,12 +6,9 @@ import pyLOM, pyLOM.NN
 sys.path.append('/gpfs/scratch/bsc21/bsc021893/parametrize/SHRED-ROM') ## Maybe change for pySHRED or add directly in pyLOM?
 from utils.processdata     import Padding, TimeSeriesDataset
 
-## Set device
-device = pyLOM.NN.select_device() # Force CPU for this example, if left in blank it will automatically select the device
-
 def split_reconstruct(Nt):
     ## Splitting into train, test and validation for reconstruction mode of SHRED
-    tridx       = np.sort(np.random.choice(Nt, size=2500, replace=False))
+    tridx       = np.sort(np.random.choice(Nt, size=int(0.7*Nt), replace=False))
     mask        = np.ones(Nt)
     mask[tridx] = 0
     vate_idx    = np.arange(0, Nt)[np.where(mask!=0)[0]]
@@ -19,11 +16,31 @@ def split_reconstruct(Nt):
     teidx       = vate_idx[1::2]
     return tridx, vaidx, teidx
 
+class TimeSeriesDatasetMine(torch.utils.data.Dataset):
+    '''
+    Input: sequence of input measurements with shape (ntrajectories, ntimes, ninput) and corresponding measurements of high-dimensional state with shape (ntrajectories, ntimes, noutput)
+    Output: Torch dataset
+    '''
+
+    def __init__(self, X, Y):
+        self.X = X
+        self.Y = Y
+        self.len = X.shape[1]
+        
+    def __getitem__(self, index):
+        return self.X[:,index,:], self.Y[:,index]
+    
+    def __len__(self):
+        return self.len
+
+## Set device
+device = pyLOM.NN.select_device() # Force CPU for this example, if left in blank it will automatically select the device
+
 ## Input parameters
 # Data paths
-podpath  = '/gpfs/scratch/bsc21/bsc021893/parametrize/latent_noncompiled_fp32_3_64_5.00e-03.npy' # Path to reduced POD basis
-senspath = '/gpfs/scratch/bsc21/bsc021893/parametrize/data_points.npz'                           # Path to sensor measurements
-sensvar  = 'velox'                                                                               # Variable from the sensor measurements we'll be working with
+senspath = 'sensors.h5' # Path to sensor measurements
+sensvar  = 'VELOX'      # Variable from the sensor measurements we'll be working with
+podvar   = 'VELOX'      # Variable from the sensor measurements we'll be working with
 
 # Output paths
 inscaler = 'out/scaler_'
@@ -31,32 +48,28 @@ ouscaler = 'out/scaler_pod.json'
 shreds   = 'out/shred_'
 
 # SHRED sensor configurations for uncertainty quantification
-sensxconfig = 3 # number of snesors per configuration
-nconfigs    = 1 # number of configurations
-
-# SHRED parameters (ask!!)
-lags   = 50 #Size of delay embedding
+nconfigs    = 1 
 
 ## Import sensor measurements
-data_points = np.load(senspath, allow_pickle=True)
-sens_vals   = data_points[sensvar] # shape (m, Nt)
+data_points = pyLOM.Dataset.load(senspath)
+sens_vals   = data_points[sensvar].astype(np.float32) # shape (m, Nt)
 nsens       = sens_vals.shape[0]
 Nt          = sens_vals.shape[1]
 
-## Split between train, test and validation: TODO: In the POD part
+## Split between train, test and validation: TODO: In the data extraction script part
 tridx, vaidx, teidx = split_reconstruct(Nt)
 
-## Import POD coefficients. Stack and rescale them. TODO: Own padding functions
-pod_coeff   = np.load(podpath)
+## Import POD coefficients. Stack and rescale them.
+pod_coeff   = pyLOM.POD.load('POD_modes_%s.h5' % podvar, vars='V')[0].T.astype(np.float32) # pyLOM actually saving VT and we need to transpose to get V
 pod_scaler  = pyLOM.NN.MinMaxScaler()
 pod_scaler.fit(pod_coeff)
 pod_scaler.save(ouscaler)
 rescaled_pod = pod_scaler.transform(pod_coeff)
 data_out     = torch.from_numpy(rescaled_pod).to(device)
-output_size  = data_out.shape[-1]
+output_size  = data_out.shape[1]
 
 ## Build SHRED architecture
-shred   = pyLOM.NN.SHRED(sensxconfig, output_size, device, nsens, nconfigs=nconfigs)
+shred   = pyLOM.NN.SHRED(output_size, device, nsens, nconfigs=nconfigs)
 
 ## Fit all SHRED configurations using the data from the sensors
 for kk, mysensors in enumerate(shred.configs):
@@ -67,12 +80,16 @@ for kk, mysensors in enumerate(shred.configs):
     myscaler.fit(myvalues)
     myscaler.save(scalpath)
     vals_config = myscaler.transform(myvalues)[np.newaxis,:,:]
-    data_in = Padding(torch.from_numpy(vals_config), lags).to(device)
+    #X = vals_config[0,:,:].T
+    #data_in = torch.from_numpy(pyLOM.math.time_delay_embedding(X)).to(device)
+    print(vals_config.shape, flush=True)
+    data_in = Padding(torch.from_numpy(vals_config)).to(device) #TODO: do our padding function for time-delay embedding
+    print(data_in.shape, data_out.shape, flush=True)
     # Generate training validation and test datasets both for reconstruction of states
-    train_dataset = TimeSeriesDataset(data_in[tridx], data_out[tridx])
-    valid_dataset = TimeSeriesDataset(data_in[vaidx], data_out[vaidx])
+    train_dataset = TimeSeriesDataset(data_in[tridx], data_out[tridx]) #TODO: use the pyLOM dataset or torch tensor dataset
+    valid_dataset = TimeSeriesDataset(data_in[vaidx], data_out[vaidx]) #TODO: use the pyLOM dataset
     # Fit SHRED
-    shred.fit(train_dataset, valid_dataset, batch_size=64, epochs=500, lr=1e-3, verbose=False, patience=100)
+    shred.fit(train_dataset, train_dataset, epochs=500, patience=100, verbose=False)
     shred.save('%s%i' % (shreds,kk), scalpath, mysensors)
 
 pyLOM.cr_info()
