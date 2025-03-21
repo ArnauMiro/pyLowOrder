d604bb2783d5360e12b5d7737a59e94f15d804c0
DavidRamosArchilla <davidrarchilla@gmail.com>
2025-03-21 09:37:01 +0100

    modified bdf converter file. Solved problem with connectivity

--- Converters/bdf2h5.py
+++ Converters/bdf2h5.py
@@ -1,37 +1,36 @@
 import pyLOM
 import numpy as np
 from pyNastran.bdf.bdf import BDF
-
-# these paths need to be set
+ 
 BDFFILE = ""
 OUTFILE = ""
-
+ 
 BDF2ELTYPE = {
-    "CTRIA3": 2,  # Triangular cell
-    "CQUAD4": 3,  # Quadrangular cell
+    'CTRIA3': 2,  # Triangular cell
+    'CQUAD4': 3,  # Quadrangular cell
 }
-
+ 
 # Load and read BDF file
 model = BDF()
 model.read_bdf(BDFFILE, xref=False)
-
+ 
 # Create needed variables
-mtype = "UNSTRUCT"  # Always unstructured
-xyz = np.array([model.nodes[nid].get_position() / 1000 for nid in model.nodes.keys()], dtype=np.float32,)
-
-conec_list = [model.elements[eid].node_ids for eid in model.elements.keys()]
-max_nodes = max(len(elem) for elem in conec_list)
-conec = np.array([elem + [-1] * (max_nodes - len(elem)) for elem in conec_list], dtype=np.int32)
-
-eltype = np.array([BDF2ELTYPE.get(model.elements[eid].type) for eid in model.elements.keys()], dtype=np.int32,)
-cellO = np.arange(len(model.elements), dtype=np.int32)
-pointO = np.array(list(model.nodes.keys()), dtype=np.int32)
-
+mtype = 'UNSTRUCT' # Always unstructured
+xyz = np.array([model.nodes[nid].get_position()/1000 for nid in model.nodes.keys()], dtype=np.float64)
+ 
+connec_list = [model.elements[eid].node_ids for eid in model.elements.keys()]
+connec_list = [[eid-1 for eid in row] for row in connec_list]
+max_nodes = max(len(elem) for elem in connec_list)
+connec = np.array([elem + [-1] * (max_nodes - len(elem)) for elem in connec_list], dtype=np.int32)
+ 
+eltype = np.array([BDF2ELTYPE.get(model.elements[eid].type) for eid in model.elements.keys()], dtype=np.int32)
+cellO = np.array(list(model.elements.keys()), dtype=np.int32) - 1
+pointO = np.array(list(model.nodes.keys()), dtype=np.int32) - 1
+ 
 # Create a mesh
-mesh = pyLOM.Mesh(mtype, xyz, conec, eltype, cellO, pointO, None)
-mesh.partition_table = pyLOM.PartitionTable.new(1, mesh.ncells, mesh.npoints)
-
-with open(OUTFILE, "w") as f:
-    mesh.save(OUTFILE)  # Store the mesh
-
+mesh = pyLOM.Mesh(mtype,xyz,connec,eltype,cellO,pointO,None)
+mesh.partition_table = pyLOM.PartitionTable.new(1,mesh.ncells,mesh.npoints)
+ 
+mesh.save(OUTFILE,mode='w') # Store the mesh, overwrite
+ 
 pyLOM.cr_info()
