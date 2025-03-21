671e8f14e9947088d8c7c86b1e24a3e52ba39d64
P. Yeste <p.yesteb@upm.es>
2025-03-21 19:06:11 +0100

    Updated example_mesh.py

--- Examples/example_mesh.py
+++ Examples/example_mesh.py
@@ -3,14 +3,14 @@ import numpy as np
 import h5py
 import pyLOM
 
-file_path = 'path-to-/clean.h5'
+file_path = '/home/p.yeste/pyLowOrder/Testsuite/DATA/CYLINDER.h5'
 
 #%%
 m = pyLOM.Mesh.load(file_path)
 print(m)
 
 #%%
-# Compute necessary geometrical information (this may take a few minutes)
+# Compute necessary geometrical information (this may take a while depending on your mesh size)
 cell_nodes = m.xyz # Cell nodes
 surf_norms = m.normal
 edge_norms = m.edge_normal
@@ -23,17 +23,18 @@ print("cell_connectivity: ", cell_connectivity.shape)
 #%%
 # Save the new data in an h5 file
 
-save_file = "/home/p.yeste/CETACEO_DATA/CLEAN/clean-mesh-vtk.hdf"
+save_file = "/home/p.yeste/CETACEO_DATA/CYLINDER_extra.h5"
 
-with h5py.File(save_file, 'r+') as f:
-    if f['VTKHDF'].require_group('CellData'):
-        del(f['VTKHDF/CellData'])
-    f['VTKHDF'].create_group('CellData')
-
-    f['VTKHDF/CellData'].create_dataset('SurfNorms', data=surf_norms)
-    f['VTKHDF/CellData'].create_dataset('EdgeNorm1', data=edge_norms[:,:3])
-    f['VTKHDF/CellData'].create_dataset('EdgeNorm2', data=edge_norms[:,3:6])
-    f['VTKHDF/CellData'].create_dataset('EdgeNorm3', data=edge_norms[:,6:])
-    f['VTKHDF/CellData'].create_dataset('CellConnectivity', data=cell_connectivity)
+# copy the original file and add the new data
+with h5py.File(file_path, 'r') as f:
+    with h5py.File(save_file, 'w') as f_save:
+        for key in f.keys():
+            f.copy(key, f_save)
+        f_save['MESH'].create_dataset('SurfNorms', data=surf_norms)
+        f_save['MESH'].create_dataset('EdgeNorm_1', data=edge_norms[:,:3])
+        f_save['MESH'].create_dataset('EdgeNorm_2', data=edge_norms[:,3:6])
+        f_save['MESH'].create_dataset('EdgeNorm_3', data=edge_norms[:,6:9])
+        f_save['MESH'].create_dataset('EdgeNorm_4', data=edge_norms[:,9:])
+        f_save['MESH'].create_dataset('CellConnectivity', data=cell_connectivity)
 
 print("Data saved in", save_file)
