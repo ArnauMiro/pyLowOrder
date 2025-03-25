93dcad978e924769c5f04da34222b3988dff2921
bef-18 <beneteiximenofranch@hotmail.com>
2025-03-25 11:43:10 -0700

    fix imports

--- pyLOM/POD/__init__.py
+++ pyLOM/POD/__init__.py
@@ -6,10 +6,10 @@
 #
 # Last rev: 09/07/2021
 
-from .wrapper import run, truncate, reconstruct
-from .utils   import extract_modes, save, load
-from .plots   import plotMode
-from ..utils  import plotResidual, plotSnapshot
+from .wrapper      import run, truncate, reconstruct
+from .utils        import extract_modes, save, load
+from .plots        import plotMode
+from ..utils.plots import plotResidual, plotSnapshot
 
 
 del wrapper, plots
