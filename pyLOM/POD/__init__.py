2d07c2f32fcee3fad32eb4c05d77966c431d6ea5
bef-18 <beneteiximenofranch@hotmail.com>
2025-04-11 12:47:06 +0200

    first version of mode clustering

--- pyLOM/POD/__init__.py
+++ pyLOM/POD/__init__.py
@@ -7,7 +7,7 @@
 # Last rev: 09/07/2021
 
 from .wrapper      import run, truncate, reconstruct
-from .utils        import extract_modes, save, load
+from .utils        import extract_modes, save, load, coherent_modes
 from .plots        import plotMode
 from ..utils.plots import plotResidual, plotSnapshot
 
