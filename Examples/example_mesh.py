4717c9479baf414ff717b32580e1781515aa3873
P. Yeste <p.yesteb@upm.es>
2025-05-06 16:09:23 +0200

    updated example mesh

--- Examples/example_mesh.py
+++ Examples/example_mesh.py
@@ -8,6 +8,7 @@ from __future__ import print_function, division
 import numpy as np
 import pyLOM
 
+file_path = './Testsuite/DATA/CYLINDER.h5'
 
 ## Load cylinder mesh
 DATAFILE = './Testsuite/DATA/CYLINDER.h5'
