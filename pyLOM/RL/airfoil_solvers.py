33d3dcf491c020fbb92fcef1420a6cce3b146788
DavidRamosArchilla <davidrarchilla@gmail.com>
2025-05-20 15:36:42 +0200

    added the possibility of check self intersections on the airfoil. xfoil now uses the wrapper

--- pyLOM/RL/airfoil_solvers.py
+++ pyLOM/RL/airfoil_solvers.py
@@ -2,7 +2,7 @@ from abc import ABC, abstractmethod
 
 import numpy as np
 import aerosandbox as asb
-from aerosandbox import XFoil
+from ._xfoil_wrapper import XFoil
 import neuralfoil as nf
 
 from pyLOM.RL import NON_CONVERGED_REWARD
@@ -24,17 +24,18 @@ class BaseSolver(ABC):
 class XFoilSolver(BaseSolver):
     """
     XFoil solver for airfoil analysis. It uses the XFoil library to compute the lift and drag coefficients. Under the hood, it uses the XFoil class from aerosandbox.
-    Requires XFoil to be on installed your machine; XFoil is available here: https://web.mit.edu/drela/Public/web/xfoil/
+    Requires XFoil to be on installed your machine; XFoil is available here: https://github.com/RobotLocomotion/xfoil/
     And xfoil to be in your PATH. You can check this by running `which xfoil` in your terminal.
     
     Args:
         alpha (float): Angle of attack in degrees.
         mach (float): Mach number.
         Reynolds (float, optional): Reynolds number. If not provided, inviscid mode is used. Default is ``None``.
+        check_selfintersection (bool, optional): If True, check for self-intersection of the airfoil when computing lift/drag. If the airfoil self-intersects, XFoil will return NON_CONVERGED_REWARD. Default is ``False``.
         xfoil_params (dict, optional): Additional parameters that are passed to the XFoil class. For example, you can set `xfoil_params={"xfoil_repanel": True}` to repanel the airfoil. Default is ``{}``.
     """
 
-    def __init__(self, alpha: float, mach: float, Reynolds: float = None, xfoil_params: dict = {}):
+    def __init__(self, alpha: float, mach: float, Reynolds: float = None, check_selfintersection: bool = False, xfoil_params: dict = {}):
         super().__init__()
         self.Reynolds = Reynolds
         self.alpha = alpha
@@ -42,9 +43,14 @@ class XFoilSolver(BaseSolver):
         self.viscous = Reynolds is not None
         if not self.viscous:
             raiseWarning("CD prediction of inviscid xfoil is poor, note that the results will not be too accurate.")
+        self.check_selfintersection = check_selfintersection
         self.xfoil_params = xfoil_params
 
     def __call__(self, airfoil: asb.Airfoil):
+        if self.check_selfintersection and self._check_if_self_intersecting(airfoil):
+            pprint(0, "Airfoil self-intersecting")
+            return self.NON_CONVERGED_REWARD
+        
         base_params = {
             "Re": 0 if not self.viscous else self.Reynolds, # 0 means inviscid mode 
             "mach": self.mach_number,
@@ -68,6 +74,16 @@ class XFoilSolver(BaseSolver):
         except Exception as e:
             pprint(0, "XFoil failed", e)
             return self.NON_CONVERGED_REWARD
+    
+    def _check_if_self_intersecting(self, airfoil, threshold=0.0005):
+        coordinates_thickness = airfoil.local_thickness()
+        # remove leading and trailing zeros from leading edge and trailing edge if they are present
+        if coordinates_thickness[0] == 0:
+            coordinates_thickness = coordinates_thickness[1:]
+        if coordinates_thickness[-1] == 0:
+            coordinates_thickness = coordinates_thickness[:-1]
+
+        return np.any(coordinates_thickness < threshold)
 
 
 class NeuralFoilSolver(BaseSolver):
