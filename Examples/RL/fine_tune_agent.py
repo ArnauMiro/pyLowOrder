3f3f805579808b97ec92384fae81f1d7985fd399
David <davidrarchilla@gmail.com>
2025-05-25 13:47:48 +0200

    added  fine tuning example

---
+++ Examples/RL/fine_tune_agent.py
@@ -0,0 +1,53 @@
+from pyLOM.RL import create_env
+from stable_baselines3 import PPO
+import torch
+
+torch.set_num_threads(1)
+
+NUM_ENVS = 8
+NEURALFOIL_ITERATIONS = 25000
+XFOIL_ITERATIONS = 15000
+THICKNESS_PENALIZATION = 0
+
+if __name__ == "__main__":
+    first_env = create_env(
+        "neuralfoil", thickness_penalization_factor=THICKNESS_PENALIZATION
+    )
+    neuralfoil_hyperparameters = {
+        "learning_rate": 2.5e-4, 
+        "n_steps": 2048, 
+        "batch_size": 64, 
+        "n_epochs": 10,
+        "gamma": 0.25,
+        "ent_coef": 0, 
+        "clip_range": 0.6,
+        "verbose": 1,
+        "policy_kwargs": {"net_arch": dict(pi=[256, 256], vf=[256, 256])},
+    }
+    initial_model = PPO(
+        "MlpPolicy", first_env, **neuralfoil_hyperparameters
+    )
+    initial_model.learn(total_timesteps=NEURALFOIL_ITERATIONS)
+    initial_model.save("pretrained_model")
+    initial_policy = initial_model.policy
+
+    second_env = create_env(
+        "xfoil", num_envs=NUM_ENVS, thickness_penalization_factor=THICKNESS_PENALIZATION
+    )
+    xfoil_hyperparameters = {
+        "learning_rate": 2.5e-4,
+        "n_steps": 512 // NUM_ENVS,
+        "batch_size": 64,
+        "n_epochs": 20,
+        "gamma": 0.3,
+        "ent_coef": 0.005, 
+        "verbose": 1,
+        "policy_kwargs": {"net_arch": dict(pi=[256, 256], vf=[256, 256])},
+    }
+    fine_tuned_model = PPO(
+        "MlpPolicy", second_env, device='cpu', **xfoil_hyperparameters
+    )
+    # load the weights of the initial policy
+    fine_tuned_model.policy.load_state_dict(initial_policy.state_dict())
+    fine_tuned_model.learn(total_timesteps=XFOIL_ITERATIONS)
+    fine_tuned_model.save("fine_tuned_model")
