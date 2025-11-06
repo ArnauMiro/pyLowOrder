723f42c35842b33d44383658411dffe6a63d1de0
bef-18 <beneteiximenofranch@hotmail.com>
2025-11-06 19:13:49 +0100

    remove plots

--- Examples/GAVI/example_02A_GAVI_R.py
+++ Examples/GAVI/example_02A_GAVI_R.py
@@ -5,22 +5,17 @@ import matplotlib.pyplot as plt
 
 # Parameters
 FILE       = 'QR.h5'
-latent_dim = 6
+latent_dim = 4
 
 # Load data
 #R = pyLOM.GAVI.load(FILE, vars=['B'])[0][:1600,:]
 R = np.load('../gavi/POD/R.npy')
 
 data,_ = pyLOM.GAVI.create_dataset(R, scale='max')
+
 vae    = pyLOM.GAVI.vae_R(data, latent_dim)
 rectra = vae.reconstruct(data)
 _,detR = vae.correlation(data)
 latent = vae.latent_space(data)
-
-for ilat in range(latent.shape[1]):
-    plt.figure(figsize=(8,6))
-    plt.plot(latent[:,ilat].cpu().numpy(), 'r')
-    plt.savefig('latents_%i.png' % ilat)
-
 
 pyLOM.cr_info()
