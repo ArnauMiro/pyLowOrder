3630cf398f12e47bbb9c9d18631dd5dd01a09f7a
bef-18 <beneteiximenofranch@hotmail.com>
2025-11-07 09:46:08 +0100

    Add header

--- Examples/GAVI/example_02A_GAVI_R.py
+++ Examples/GAVI/example_02A_GAVI_R.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python
+#
+# Example on how to run fit an autoencoder for the temporal dynamics of a system following the GAVI methodology.
+#
+# Eiximeno, B., A., Miró, A., Kutz, J. N., Rodriguez, I., & Lehmkuhl, O. (2025). 
+# On the integration of geometry agnostic variational-autoencoders into large-scale SVD based models. 
+# Computers & Fluids, 302, 106797. https://doi.org/10.1016/j.compfluid.2025.106797
+#
+# Last revision: 07/11/2025
+import torch
 import pyLOM.NN
 
 # Parameters
@@ -5,7 +15,7 @@ FILE       = 'QR.h5'
 latent_dim = 4
 
 # Load data
-R = pyLOM.NN.GAVI.load(FILE, vars=['B'])[0][:1600,:]
+R = pyLOM.NN.GAVI.load(FILE, vars=['B'])[0]
 
 data,_ = pyLOM.NN.GAVI.create_dataset(R, scale='max')
 
