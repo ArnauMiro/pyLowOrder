be96d07bd48e8103d5b7fbed9d301d8ac9b0621c
bef-18 <beneteiximenofranch@hotmail.com>
2025-11-18 08:58:05 +0100

    reconstruct 2 variables

--- Examples/GAVI/example_03_decompress_Q.py
+++ Examples/GAVI/example_03_decompress_Q.py
@@ -4,7 +4,8 @@ import pyLOM, pyLOM.NN
 
 ## Input files
 fmesh = '../pylom_mesh.h5'
-fr    = 'QR.h5'
+frx   = 'QR_velox.h5'
+fry   = 'QR_veloy.h5'
 fcomp = 'compressed_data_array.h5'
 
 ## Load mesh
@@ -13,19 +14,27 @@ nelxAE  = pOrder**3
 mesh    = pyLOM.Mesh.load(fmesh)
 
 ## Load R
-R    = pyLOM.NN.GAVI.load(fr, vars=['B'])[0]
-nmod = R.shape[0]
+Rx   = pyLOM.NN.GAVI.load(frx, vars=['B'])[0]
+Ry   = pyLOM.NN.GAVI.load(fry, vars=['B'])[0]
+nmod = Rx.shape[0]
 
 ## Load compressed data (will be already on the needed DEVICE)
 Qmeans, Qstds, weights, biases, Qs, Bs = pyLOM.NN.GAVI.load_compressed(fcomp, mesh.partition_table, nelxAE=nelxAE)
 
-## Inference and reconstruct Q
-Q = pyLOM.NN.GAVI.reconstruct_Q(mesh, nelxAE, nmod, Qmeans, Qstds, weights, biases, Qs, Bs)
+## Inference and reconstruct Qx to do velox
+Qx = pyLOM.NN.GAVI.reconstruct_Q(mesh, nelxAE, nmod, Qmeans, Qstds, weights, biases, Qs, Bs, ivar=0)
+velox = pyLOM.math.matmul(Qx, Rx[:,0])
+del Qx
+pyLOM.pprint(0, 'velox is reconstructed', flush=True)
 
-## Reconstruct and visualize the first snapshot
-reco = pyLOM.math.matmul(Q, R[:,0])
-d    = pyLOM.Dataset(xyz=mesh.xyz, ptable=mesh.partition_table, order=mesh.pointOrder, point=True, vars={'time':{'idim':0,'value':np.array([0.])}}, VELOX={'ndim':1,'value':reco[:,np.newaxis]})
-pyLOM.io.pv_writer(mesh,d.to_cpu(['VELOX']),'flow',basedir='out/flow',instants=np.array([0]),times=np.array([0.]),vars=['VELOX'],fmt='vtkh5')
+## Inference and reconstruct Qy to do veloy
+Qy = pyLOM.NN.GAVI.reconstruct_Q(mesh, nelxAE, nmod, Qmeans, Qstds, weights, biases, Qs, Bs, ivar=1)
+veloy = pyLOM.math.matmul(Qy, Ry[:,0])
+del Qy
+pyLOM.pprint(0, 'veloy is reconstructed', flush=True)
 
+## Build the dataset and write to paraview
+d     = pyLOM.Dataset(xyz=mesh.xyz, ptable=mesh.partition_table, order=mesh.pointOrder, point=True, vars={'time':{'idim':0,'value':np.array([0.])}}, VELOX={'ndim':1,'value':velox[:,np.newaxis]}, VELOY={'ndim':1,'value':veloy[:,np.newaxis]})
+pyLOM.io.pv_writer(mesh,d.to_cpu(['VELOX','VELOY']),'flow',basedir='out/flow',instants=np.array([0]),times=np.array([0.]),vars=['VELOX','VELOY'],fmt='vtkh5')
 
 pyLOM.cr_info()
