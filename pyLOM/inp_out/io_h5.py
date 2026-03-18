7c478b8babe4fdaaecbef7d5c94df5f77b67f92a
Arnau Miro <arnau.miro@upc.edu>
2026-03-18 09:36:09 +0100

    Test

--- pyLOM/inp_out/io_h5.py
+++ pyLOM/inp_out/io_h5.py
@@ -196,10 +196,10 @@ def h5_load_meshes(file,ptable,repart):
 		inods = np.arange(istart,iend,dtype=np.int32)
 	xyz    = np.array(file['xyz'][inods,:],file['xyz'].dtype) 
 	pointO = np.array(file['pointOrder'][inods],np.int32)
-#	# Fix the connectivity to start at zero
-#	conec2 = -np.ones_like(conec).flatten()# This is a 1D array of -1 of the size of our connectivity
-#	conec2[conec.flatten() >= 0] = np.searchsorted(pointO, conec[conec >= 0].flatten()) # Search only the positive values
-#	conec = conec2.reshape(conec.shape).astype(np.int32) # Reshape the connectivity to its original format
+	# Fix the connectivity to start at zero
+	conec2 = -np.ones_like(conec).flatten()# This is a 1D array of -1 of the size of our connectivity
+	conec2[conec.flatten() >= 0] = np.searchsorted(pointO, conec[conec >= 0].flatten()) # Search only the positive values
+	conec = conec2.reshape(conec.shape).astype(np.int32) # Reshape the connectivity to its original format
 	# Return
 	return mtype, xyz, conec, eltype, cellO, pointO
 
