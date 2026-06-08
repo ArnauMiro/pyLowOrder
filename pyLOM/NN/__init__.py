21dd3f9fdf1b83c108b12429ad049d54222b8ada
fermingut <fermin.gutierrezm@upm.es>
2026-06-08 16:37:25 +0200

    Add RBFELM import to NN module

--- pyLOM/NN/__init__.py
+++ pyLOM/NN/__init__.py
@@ -49,6 +49,7 @@ from .architectures.encoders_decoders import Encoder1D, Decoder1D, Encoder2D, Fu
 from .architectures.pinn              import PINN, BurgersPINN, Euler2DPINN, NavierStokesIncompressible2DPINN, BoundaryCondition
 from .architectures.shred             import SHRED
 from .architectures.binary_classifier import BinaryClassifier
+from .architectures.rbfelm            import RBFELM
 
 from .utils                           import plot_train_test_loss, plot_true_vs_pred, save_experiment_artifacts
 from ..utils.plots                    import plotSnapshot, plotModalErrorBars, plotTimeSeries
